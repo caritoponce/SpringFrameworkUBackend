da5f6f760e3787fef78b95a390bbde4a4e33870a
caritoponce <lapaisacarito@gmail.com>
2021-09-20 11:47:46 -0500

    New Project Spring Framework Udemy Backend

---
+++ src/main/java/com/bolsadeideas/springboot/di/app/models/domain/ItemFactura.java
@@ -0,0 +1,40 @@
+package com.bolsadeideas.springboot.di.app.models.domain;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ItemFactura {
+	
+	@Autowired
+	private Producto producto;
+	
+	private Integer cantidad;
+	
+	public ItemFactura(Producto producto, Integer cantidad) {
+
+		this.producto = producto;
+		this.cantidad = cantidad;
+	}
+
+	public Producto getProducto() {
+		return producto;
+	}
+
+	public void setProducto(Producto producto) {
+		this.producto = producto;
+	}
+
+	public Integer getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(Integer cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	
+	public Integer calcularImporte() {
+		return cantidad*producto.getPrecio();
+	}
+	
+
+}
